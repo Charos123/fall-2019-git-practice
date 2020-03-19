f496d2d04148c60aea7b74389282df1e066129bc
Charos <charossharipova@chaross-MacBook-Pro.local>
2020-03-18 20:08:24 -0400

    First class

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println(" brave new world");
         System.out.println("Hello world");
+        System.out.println("hey hey");
+        System.out.println("wow wow");
     }
 }
