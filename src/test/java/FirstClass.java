2508c526394afb127472b64577926ab6b354696b
Charos123 <58119587+Charos123@users.noreply.github.com>
2020-03-25 19:55:44 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println(" brave new world");//comment
-        System.out.println("Hello world");
+        System.out.println(" brave new world");
+        System.out.println("Hello world");//comment
         System.out.println("hey hey");
         System.out.println("wow wow");
         System.out.println("change two");
