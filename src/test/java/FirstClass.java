efdbc718b6b43d8c6c58f1284d78ccf0177caf47
Charos <charossharipova@chaross-MacBook-Pro.local>
2020-03-26 11:47:49 -0400

    I did a lot of work

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -4,12 +4,16 @@ public class FirstClass {
         System.out.println("Hello world");//comment
         System.out.println("hey hey");
         System.out.println("wow wow");//another comment
+        System.out.println("hey hey");//another comment
+        System.out.println("wow wow");
         System.out.println("change two");
         System.out.println("change three");
      
           //helle hello
          //second change
         //third change
+        System.out.println("working hard");
+        System.out.println("great job");
 
        
     }
